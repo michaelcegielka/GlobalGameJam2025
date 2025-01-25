6a43044cc8c97d1d9d81f846540f5dbed93cab46
SoldatenEnte <75547159+SoldatenEnte@users.noreply.github.com>
2025-01-25 12:15:16 +0100

    dirt shader mask test

---
+++ gamejam2025/Objects/erase_test_mesh.gd
@@ -0,0 +1,51 @@
+extends MeshInstance3D
+
+@onready var player: CharacterBody3D = $"../Player"
+
+var mask_image: Image
+var mask_texture: ImageTexture
+
+func _ready():
+	if !player:
+		push_error("Player not assigned!")
+		return
+	
+	var width = 2048
+	var height = 2048
+	mask_image = Image.create(width, height, false, Image.FORMAT_RGBA8)
+	mask_image.fill(Color(1, 1, 1, 1))
+	mask_texture = ImageTexture.create_from_image(mask_image)
+
+	material_overlay.set("shader_param/mask_texture", mask_texture)
+
+func _process(delta):
+	if player.current_state == player.States.GROUNDED:
+		var player_position = player.global_position
+		erase_dirt(player_position)
+
+func erase_dirt(position: Vector3):
+	var uv_position = world_to_texture_coords(position)
+	var radius = 20
+	var radius_sq = radius * radius
+	
+	for x in range(-radius, radius):
+		for y in range(-radius, radius):
+			if x*x + y*y <= radius_sq:
+				var pixel_pos = uv_position + Vector2(x, y)
+				if pixel_pos.x >= 0 && pixel_pos.x < mask_image.get_width() && pixel_pos.y >= 0 && pixel_pos.y < mask_image.get_height():
+					mask_image.set_pixel(pixel_pos.x, pixel_pos.y, Color(0, 0, 0, 0))
+	
+	mask_texture.update(mask_image)
+	
+func world_to_texture_coords(world_position: Vector3) -> Vector2:
+	var mesh_scale = global_transform.basis.get_scale()
+	var local_position = to_local(world_position) / mesh_scale
+	
+	var uv_x = (local_position.x / 100.0) + 0.5
+	var uv_y = (local_position.z / 100.0) + 0.5
+	
+	
+	uv_x *= mask_image.get_width()
+	uv_y *= mask_image.get_height()
+	
+	return Vector2(uv_x, uv_y)
